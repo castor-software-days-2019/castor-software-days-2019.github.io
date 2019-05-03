34288f657c0555b85b632501833f457db7c3994a
Javier Cabrera Arteaga <xppcoder@gmail.com>
2019-05-03 11:49:27 +0200

    FIX: hashtag navigation script

--- js/agency.js
+++ js/agency.js
@@ -9,8 +9,10 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+            scrollTop: $($anchor.attr('to')).offset().top
+        }, 1500, 'easeInOutExpo', () => {
+            window.location = $anchor.attr('to')
+        });
         event.preventDefault();
     });
 });
